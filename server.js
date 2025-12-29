83dd119d63fe1bf6437b52443ec14e67f3d4b5d2
maksud rizan <rizanmahmudexample.com>
2025-12-29 22:15:58 +0400

    add port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true}));
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
